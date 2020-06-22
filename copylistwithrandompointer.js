f380078769dc9bce1d2eb8812b1d5d42786c4f90
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-22 08:52:57 -0400

    edits to solution

--- copylistwithrandompointer.js
+++ copylistwithrandompointer.js
@@ -1,12 +1,14 @@
 //Objective is to make a deep copy of a linked list with nodes that have a
 //'next' and 'random' pointer
 
-//A Node has the following construct:
-//function Node(val, next, random) {
-//   this.val = val;
-//   this.next = next;
-//   this.random = random;
-//}
+class Node {
+    constructor(val, next, random) {
+        this.val = val 
+        this.next = next 
+        this.random = random
+    }
+}
+
 
 //O(n) solution that iterates over the linked list twice
 //Once to set the values and once to set the pointers
@@ -18,12 +20,16 @@ if (!head) {
     return null 
 }
 
+//First iteration over the linked list to make a deep copy of the node
+//and have a reference to itself
 let curr = head
 while (curr) {
     dic.set(curr, new Node(curr, null, null))
     curr = curr.next
 }
 
+//Second iteration over the linked list to give each node the correct
+//next and random pointers
 curr = head
 while (curr) {
     dic.get(curr).next = dic.get(curr.next)
